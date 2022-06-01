bc58cfd18d0a0c4dc8cec060f3c64e6f7bfb1002
CoderWanFeng <1529577833@qq.com>
2022-06-01 12:26:35 +0800

    更新👉vuepress

---
+++ vuepress/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    title: 'python-office',// 设置网站标题
+    description: 'python-office自动化办公',
+    base: '/docs',// 设置站点根路径
+    dest: './docs',  // 设置输出目录
+    head: [],
+    plugins: [],
+    themeConfig: {
+        // 添加导航栏
+        nav: [
+            { text: '主页', link: '/' },
+            { text: 'GitHub', link: 'https://github.com/CoderWanFeng/python-office' },
+            { text: '视频教程', link: 'https://www.bilibili.com/video/BV1pT4y1k7FH' },
+        ],
+        lastUpdated: 'Last Updated',
+        sidebar:[
+            '/',
+            '/office/office',
+            '/office/word',
+            '/office/excel',
+        ],
+    }
+}
---
+++ vuepress/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: http://python4office.cn/images/github-nav.jpg
+heroText: Welcome to python-office
+tagline: Born to win.
+actionText: 开始探索之旅 →
+actionLink: /office/office
+features:
+- title: 关于python-office
+  details: Python-office 是一个 Python 自动化办公第三方库，能解决大部分自动化办公的问题。
+- title: 视频教程
+  details: B站：https://www.bilibili.com/video/BV1pT4y1k7FH
+- title: 一行代码
+  details: 实现自动化办公，做到开箱即用
+footer: Hep Licensed | Copyright © 2022-present 程序员晚枫
+---
